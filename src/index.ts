a822689d0be95b6a121724fbc2d7e3ae23fceae9
scroobius-pip <sim04ful@gmail.com>
2020-01-29 22:21:57 +0100

    finished graphql schema

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { ApolloServer, gql } from 'apollo-server-micro'
+import schema from './schema/schemaString'
+const gqlSchema = gql(schema)
+const microCors = require('micro-cors')
+const cors = microCors({
+    allowHeaders: ['X-Requested-With', 'Access-Control-Allow-Origin',
+        'X-HTTP-Method-Override', 'Content-Type',
+        'Authorization', 'Accept', 'token']
+})
+
+const server = new ApolloServer({
+    playground: true,
+    introspection: true,
+    mocks: true,
+    typeDefs: gqlSchema,
+})
+
+// @ts-ignore
+export default cors((req, res) => {
+    if (req.method === 'OPTIONS') {
+        res.end()
+        return
+    }
+    return server.createHandler()(req, res)
+})
---
+++ src/schema/generateSchemaString.ts
@@ -0,0 +1,15 @@
+import schema from './'
+import fs = require('fs')
+
+(async () => {
+    const data = `
+    const data = \`${await (schema)}\`
+    export default data
+    `
+    console.log('Generating Schema String')
+
+    fs.writeFile('./src/schema/schemaString.ts', data, (err) => {
+        if (err) console.error(err)
+        console.log('Successfully Generated Schema String')
+    })
+})()
